a2c59fd15cd34715aef0ef622accd3827094890a
gqueirozcruz <105554391+gqueirozcruz@users.noreply.github.com>
2022-07-16 13:12:18 -0300

    Arquitetura_VMC

--- Main.py
+++ Main.py
@@ -1,16 +1,40 @@
 
 # View - o que vai para o usuário 
-usuario = input("informe  o seu usuario")
-senha_digitado = input("informe sua senha")
+def view ():
+    usuario_digitado = input("informe  o seu usuario")
+    senha_digitado = input("informe sua senha")
+    controller(usuario_digitado, senha_digitado)
+
+
+
+
 
 # Model - O que vem do banck de dados (BD)
-usuario_BD = 'joao'
-senha_BD = '123'
+def model_usuario():
+    usuario_BD = 'joao'
+    return usuario_BD
+
+
+def model_senha():
+    senha_BD = '123'
+    return senha_BD 
+
+
+
 
 # Constrolller - a validação 
+def controller(usuario_digitado, senha_digitado):
+    usuario_BD = model_usuario()
+    senha_BD = model_senha()
 
-if usuario == usuario_BD and senha_digitado == senha_BD:
-    print("pode entrar")
-else: 
-    print("usuário ou senha inválido")
+    if usuario_digitado == usuario_BD and senha_digitado == senha_BD:
+        print("pode entrar")
+    else: 
+        print("usuário ou senha inválido")
 
+
+
+
+
+
+view()
